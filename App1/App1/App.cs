ed21ca59111c70626324eac385d2cbab87d23b84
furuya02 <sin@sapporoworks.ne.jp>
2015-11-12 06:17:41 +0900

    initialize

---
+++ App1/App1/App.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App1 {
+    public class App : Application {
+        public App() {
+            MainPage = new MyPage();
+        }
+    }
+    ///////////////////////////////////////////////////////////////////
+    //
+    // DisplayAlertのサンプル
+    //
+    ///////////////////////////////////////////////////////////////////
+    class MyPage : ContentPage {
+        public MyPage() {
+            //ボタンの生成
+            var button = new Button() {
+                Text = "Button"
+            };
+            //ボタンを押した際のイベント
+            button.Clicked += (s, e) => {
+                //DisplayAlertの表示
+                DisplayAlert("タイトル", "メッセージ", "OK");
+            };
+
+            //ボタンを押した際のイベント
+            //button.Clicked += async (s, e) => {
+            //    //DisplayAlertの表示
+            //    var result = await DisplayAlert("タイトル", "選択してください", "OK", "キャンセル");
+            //    //戻り値をボタンテキストにセットする
+            //    button.Text = result.ToString();
+            //};
+
+            Content = button;
+        }
+
+    }
+
+
+    /*
+    ///////////////////////////////////////////////////////////////////
+    //
+    // DisplayActionSheetのサンプル
+    //
+    ///////////////////////////////////////////////////////////////////
+    class MyPage : ContentPage {
+        public MyPage() {
+            //ボタンの生成
+            var button = new Button() {
+                Text = "Button"
+            };
+            //ボタンを押した際のイベント
+            button.Clicked += async (s, e) => {
+                //DisplayActionSheetの表示
+                var retult = await DisplayActionSheet("選択してください", "キャンセル", "削除", "編集", "コピー", "貼り付け");
+                //返された文字列でボタンテキストを書き換える
+                button.Text = retult;
+            };
+            Content = new StackLayout {
+                Padding = new Thickness(0, 20, 0, 0),//上に余白を持つ
+                Children = { button, }//スタックレイアウトにボタンを１つだけ持つ
+            };
+        }
+    }
+    */
+}
